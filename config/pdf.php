3666b9fdae15e9eb80970168d2f360199ea124b0
Candra Lahagu <candralahagu303@gmail.com>
2025-05-18 04:23:50 -0700

    push

--- config/pdf.php
+++ config/pdf.php
@@ -1,15 +1,15 @@
 <?php
 
 return [
-	'mode'                  => 'utf-8',
-	'format'                => 'A4',
-	'author'                => '',
-	'subject'               => '',
-	'keywords'              => '',
-	'creator'               => 'Laravel Pdf',
-	'display_mode'          => 'fullpage',
-	'tempDir'               => base_path('../temp/'),
-	'pdf_a'                 => false,
-	'pdf_a_auto'            => false,
-	'icc_profile_path'      => ''
+	'mode'              => 'utf-8',
+	'format'            => 'A4',
+	'author'            => 'SMP Betzata Leilem',
+	'subject'           => 'Laporan Sekolah',
+	'keywords'          => 'SMP, Betzata, Laporan, PDF',
+	'creator'           => 'Sistem Informasi Sekolah',
+	'display_mode'      => 'fullpage',
+	'tempDir'           => storage_path('app/temp'),
+	'pdf_a'             => false,
+	'pdf_a_auto'        => false,
+	'icc_profile_path'  => ''
 ];
